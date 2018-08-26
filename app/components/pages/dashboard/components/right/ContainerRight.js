73657a5a529ac9cd247c48c5bf20dd454c35a196
linhtd <linh.trinh@fss.com.vn>
2018-08-27 00:58:58 +0700

    fix lỗi k install dc

--- app/components/pages/dashboard/components/right/ContainerRight.js
+++ app/components/pages/dashboard/components/right/ContainerRight.js
@@ -2,12 +2,15 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import Comment from './components/Comment'
 import Chart from './components/Chart'
+import GaugeChart from './components/ChartsSatisfy'
+// and just use it
 class DashBoard extends React.Component{
 
   render(){
      return(
           <div className=" right" >
-              <Comment />
+              {/* <Comment /> */}
+              <GaugeChart />
               <Chart />
 
 
