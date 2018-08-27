776ea7df31e0d12e07b86f753a396cab5179bae4
LongND <nguyenduclong12a2nd@gmail.com>
2018-08-28 05:12:49 +0700

    remove bundel

--- app/components/pages/dashboard/components/right/components/ChartsSatisfy.js
+++ app/components/pages/dashboard/components/right/components/ChartsSatisfy.js
@@ -6,7 +6,19 @@ class Comment extends React.Component{
 	render () {
   	return (
       <div className="comment col-md-12 module" >
-              <ReactSpeedometer />
+            <ReactSpeedometer
+            value={55}
+            maxValue={100}
+            needleColor="#002864"
+            startColor="#ff0000"
+            segments={10}
+            endColor="#4e7ea9"
+            textColor="#0092f1"
+            needleTransitionDuration={100}
+            needleTransition="easeElastic"
+            currentValueText="Chỉ số hài lòng: ${value}"
+            />
+
               </div>
     );
   }
