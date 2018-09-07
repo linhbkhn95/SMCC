e4d3a5c266ab920824fb9c9695251c5519851391
linhbkhn95 <linh.trinh@fss.com.vn>
2018-09-07 15:43:12 +0700

    up

--- app/components/pages/dashboard/DashBoard.js
+++ app/components/pages/dashboard/DashBoard.js
@@ -14,7 +14,7 @@ class DashBoard extends React.Component{
              </div>
              <div className="col-md-4">
                 <div className="map">   </div>
-                <Center />
+                {/* <Center /> */}
              </div>
              <div className="col-md-5 remove-padding-col">
                 <ContainerRight />
